6977d6b34add7d524c49c7fc55c300fb17a30a6c
Lee Hudson <lee.hudson@diamond.ac.uk>
2019-08-23 10:02:15 +0100

    Move dls_pcs_asyn_axis into pcsAxis in builder.

--- etc/builder.py
+++ etc/builder.py
@@ -6,6 +6,9 @@ from iocbuilder.modules.motor import MotorLib, MotorRecord
 from iocbuilder.modules.calc import Calc
 from iocbuilder.modules.busy import Busy
 
+
+
+
 class pcsController(Device):
     """Defines the device configuration"""
 
@@ -43,22 +46,31 @@ class pcsController(Device):
         POLLMOVING = Simple("Poll period (ms) while moving", int),
         POLLNOTMOVING = Simple("Poll period (ms) while not moving", int))
 
+class _dls_pcs_asyn_motor(AutoSubstitution,MotorRecord):
+    TemplateFile = "dls_pcs_asyn_motor.db"
+
+
 class pcsAxis(Device):
     """Defines the device configuration"""
-
-    Dependencies = (Asyn, MotorLib)
-
-    # Libraries
-    #LibFileList = ['WBPcs8000']
-    #DbdFileList = ['WBPcs8000']
+    WarnMacros = False
+    Dependencies = (Asyn, MotorLib,Busy, Calc)
 
 
     # Constructor, just store parameters
-    def __init__(self, CONTROLLER, AXIS_NO, **args):
+    def __init__(self, CONTROLLER, AXIS_NO,P,M,ADDR,DESC,MRES,VELO,PREC,EGU,TWV, **args):
         Device.__init__(self)
         self.CONTROLLER = CONTROLLER
         self.AXIS_NO = AXIS_NO
-
+        self.P = P
+        self.M = M
+        self.ADDR = ADDR
+        self.DESC = DESC
+        self.MRES = MRES
+        self.VELO = VELO
+        self.PREC = PREC
+        self.EGU = EGU
+        self.TWV = TWV
+        _dls_pcs_asyn_motor(P=self.P,M=self.M,PORT=self.CONTROLLER,ADDR=self.ADDR,DESC=self.DESC,MRES=self.MRES,VELO=self.VELO,PREC=self.PREC,EGU=self.EGU,TWV=self.TWV)
     # Once per instantiation
     def Initialise(self):
         print "# Configure Walter and Bai PCS8000 Axis"
@@ -68,12 +80,14 @@ class pcsAxis(Device):
     # Arguments
     ArgInfo = makeArgInfo(__init__,
                           CONTROLLER = Simple("Controller name", str),
-                          AXIS_NO = Simple("Axis number", int))
+                          AXIS_NO = Simple("Axis number", int),
+                          P = Simple("Axis number", str),
+                          M = Simple("Axis number", str),
+                          ADDR = Simple("Axis number", int),
+                          DESC = Simple("Axis number", str),
+                          MRES = Simple("Axis number", float),
+                          VELO = Simple("Axis number", float),
+                          PREC = Simple("Axis number", int),
+                          EGU = Simple("Axis number", str),
+                          TWV = Simple("Axis number", float))
 
-class dls_pcs_asyn_motor(AutoSubstitution, MotorRecord):
-    WarnMacros = False
-    TemplateFile = 'dls_pcs_asyn_motor.db'
-    Dependencies = (Busy, Calc)
-    def __init__(self, **kwargs):
-        # Pass down the common parameters
-        self.__super.__init__(**kwargs)
