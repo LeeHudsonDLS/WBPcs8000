9c7c86a35ecc035b98472537a739ff9af7444ba5
Lee Hudson <lee.hudson@diamond.ac.uk>
2019-10-16 14:26:56 +0100

    Added a work around to get seq xml looking right for TR6

--- WBPcs8000App/src/Sequencer.cpp
+++ WBPcs8000App/src/Sequencer.cpp
@@ -7,45 +7,45 @@
 Sequencer::Sequencer(const std::string& xmlTemplateFile) {
 
     pDoc = xmlParseMemory(xmlTemplateFile.c_str(),xmlTemplateFile.size());
-	if (pDoc == NULL ) {
-		printf("Document not parsed successfully. \n");
-		//Some error handling
-	}else{
-		printf("XML parsed successfully. \n");
-	}
+    if (pDoc == NULL ) {
+        printf("Document not parsed successfully. \n");
+        //Some error handling
+    }else{
+        printf("XML parsed successfully. \n");
+    }
 
 }
 
 int Sequencer::setElement(const std::string& xPath, const std::string& value) {
 
-	xmlChar *xpath = (xmlChar*) xPath.c_str();
-	xmlXPathContextPtr context;
+    xmlChar *xpath = (xmlChar*) xPath.c_str();
+    xmlXPathContextPtr context;
 
-	context = xmlXPathNewContext(pDoc);
+    context = xmlXPathNewContext(pDoc);
     if (context == NULL) {
         printf("Error in xmlXPathNewContext\n");
         return -1;
-	}
+    }
     searchResult = xmlXPathEvalExpression(xpath, context);
-	xmlXPathFreeContext(context);
-	if (searchResult == NULL) {
-		printf("Error in xmlXPathEvalExpression\n");
-		return -1;
-	}
-	if(xmlXPathNodeSetIsEmpty(searchResult->nodesetval)){
-		xmlXPathFreeObject(searchResult);
-                printf("No result\n");
-		return -1;
-	}
+    xmlXPathFreeContext(context);
+    if (searchResult == NULL) {
+        printf("Error in xmlXPathEvalExpression\n");
+        return -1;
+    }
+    if(xmlXPathNodeSetIsEmpty(searchResult->nodesetval)){
+        xmlXPathFreeObject(searchResult);
+        printf("No result\n");
+        return -1;
+    }
 
-	if (searchResult) {
-		nodeset = searchResult->nodesetval;
+    if (searchResult) {
+        nodeset = searchResult->nodesetval;
 
         //Set the value
         xmlNodeSetContent(nodeset->nodeTab[0],(const xmlChar*)value.c_str());
 
-		xmlXPathFreeObject (searchResult);
-	}
+        xmlXPathFreeObject (searchResult);
+    }
 
 }
 
@@ -66,8 +66,25 @@ int Sequencer::setElement(const std::string& xPath, const double& value) {
 
 std::string Sequencer::getXml() {
     xmlChar* xmlbuff;
-    int buffersize;
+    int buffersize,pos;
+    std::string xmlHeader = "<?xml version=\"1.0\"?>\n";
+    std::string definitionsElement = "<definitions/>";
+    std::string startElement = "<start/>";
+    std::string commandString;
 
-    xmlDocDumpFormatMemory(pDoc, &xmlbuff, &buffersize, 1);
-    return (char*)xmlbuff;
+    xmlDocDumpFormatMemory(pDoc, &xmlbuff, &buffersize, 0);
+    commandString = (char*)xmlbuff;
+
+
+    //Remove XML header
+    while ((pos = commandString.find(xmlHeader)) != std::string::npos)
+        commandString.erase(pos, xmlHeader.size());
+
+    while ((pos = commandString.find(definitionsElement)) != std::string::npos)
+        commandString.replace(pos, definitionsElement.size(),"<definitions></definitions>");
+
+    while ((pos = commandString.find(startElement)) != std::string::npos)
+        commandString.replace(pos, startElement.size(),"<start></start>");
+
+    return commandString;
 }
